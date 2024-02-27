b341b9b77b10f7bceb99355a365a179750b30654
nevilia <winter04nevilia@gmail.com>
2024-02-27 10:53:09 +0530

    backend init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
 // Middleware
 app.use(cors());
@@ -15,7 +16,7 @@ mongoose.connect(MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Error connecting to MongoDB:', error));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
